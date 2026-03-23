138695a57071e98b9477cd0ae12f19d7716e26af
23thirokawaberry-sudo <23thirokawaberry@wakatipu.school.nz>
2026-03-24 11:44:50 +1300

    3/24/2026

--- Untitled-1.py
+++ Untitled-1.py
@@ -24,28 +24,44 @@ def validation(value):
         return valid
     except ValueError:
         return "X"
+    
 
-def add_student(name, year, credits): 
-    students[name] = {"year": year, "credits": credits}
+def add_student(name, year): 
+    students[name] = {"year": year, "credits": [0, 0, 0, 0]}
 
-def add_student_credits(NA, A, M, E):
-    credits = [NA, A, M, E]
-    return credits
+def add_student_credits(lists, grade, amount):
+    lists[grade] += amount
+
+def grade_loop(loopcount):
+    if loopcount == 0:
+        return "Not achieved"
+    elif loopcount == 1:
+        return "Achieved"
+    elif loopcount == 2:
+        return "Merit"
+    elif loopcount == 3:
+        return "Excellence"
+    
 
 def display_info():
     for student, info in students.items():
         print(student)
-        for detail in info:
-            print(detail)
+        for stat in info.values():
+            print(stat)
 
 '''
 INTERFACE CODE -----------------------------------------------------------------------
 '''
 while interface == True:
-    task = validation(input("What would you like to do? \n1. View students | 2. Add student | 3. End task"))
+    task = validation(input("What would you like to do? \n1. View students | 2. Add student | 3. End task" ))
     if task == 1:
         display_info()
-
+    elif task == 2:
+        name = input('What name is the new student? ')
+        year = validation(input("What year level is the new student? "))
+        add_student(name, year)
+        for i in range(4):
+            add_student_credits(students[name]["credits"], i, validation(input(f"How many {grade_loop(i)}s does {name} have? ")))
     elif task == 3:
         interface == False
-    else:print("Invalid number")
+    else:print("Invalid input")
